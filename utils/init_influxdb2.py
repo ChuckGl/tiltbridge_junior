5dd09b0ef96021e4ae20a16fecb5b7694e5108dc
ChuckGl <39985823+ChuckGl@users.noreply.github.com>
2024-03-15 19:26:44 +0100

    Update init_influxdb2.py

--- utils/init_influxdb2.py
+++ utils/init_influxdb2.py
@@ -4,7 +4,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS, WritePrecision
 
 # Define InfluxDB connection parameters
 INFLUXDB2_URL = "http://localhost:8086"
-INFLUXDB2_TOKEN = "gmHXhCVjSdiVLrtMLXwB0cvM90C9-eb5JGL6hoMLm0a5ZxjUSEXmqtDUfEy3EyIPHUn8fIAOgjnW65sRpHqBgg=="
+INFLUXDB2_TOKEN = "SecretToken"
 INFLUXDB2_ORG = "brewhouse"
 INFLUXDB2_BUCKET = "fermenter"
 
